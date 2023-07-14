76d7813cdaee28e0584addc034a7250c29b516e4
Mr RAAN <hiafraan@gmail.com>
2023-07-14 20:44:54 +0530

    applying-css-styles

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -4,11 +4,11 @@ import Footer from "./Footer";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
       <Content />
       <Footer />
-    </>
+    </div>
   );
 }
 
