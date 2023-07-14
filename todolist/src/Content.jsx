53ef5ecde0d28c9dc95b81d5b79646a221a496bf
Mr RAAN <hiafraan@gmail.com>
2023-07-14 21:33:36 +0530

    useState Hook

--- todolist/src/Content.jsx
+++ todolist/src/Content.jsx
@@ -1,27 +1,31 @@
-import React from "react";
+import { useState } from "react";
 
 const Content = () => {
+  const [name, setName] = useState("Raan");
+  const [count, setCount] = useState(0);
+
   const handleNameChange = () => {
     const names = ["Husni", "Kiyas", "Raan"];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    setName(names[int]);
   };
 
   const handleClick = () => {
-    console.log("You clicked it");
+    setCount(count + 1);
+    setCount(count + 1);
+    console.log(count);
   };
-  const handleClick2 = (name) => {
-    console.log(`${name} was clicked`);
+
+  const handleClick2 = () => {
+    console.log(count);
   };
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText);
-  };
+
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>{" "}
+      <p>Hello {name}!</p>
+      <button onClick={handleNameChange}>Change Name</button>
       <button onClick={handleClick}>Click It</button>
-      <button onClick={() => handleClick2("Mr RAAN")}>Click It</button>
-      <button onClick={(e) => handleClick3(e)}>Click It</button>
+      <button onClick={handleClick2}>Click It</button>
     </main>
   );
 };
