d0e2023818dd7e5c069035f86469dda39a2e721c
Mr RAAN <hiafraan@gmail.com>
2023-07-14 21:09:41 +0530

    click-events

--- todolist/src/Content.jsx
+++ todolist/src/Content.jsx
@@ -6,7 +6,24 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
-  return <main>Hello {handleNameChange()}!</main>;
+
+  const handleClick = () => {
+    console.log("You clicked it");
+  };
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+  return (
+    <main>
+      <p>Hello {handleNameChange()}!</p>{" "}
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={() => handleClick2("Mr RAAN")}>Click It</button>
+      <button onClick={(e) => handleClick3(e)}>Click It</button>
+    </main>
+  );
 };
 
 export default Content;
