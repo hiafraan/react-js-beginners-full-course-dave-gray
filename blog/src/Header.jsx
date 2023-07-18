4bcecf7317c57e45aa607be62a40088d42a17d1c
Mr RAAN <hiafraan@gmail.com>
2023-07-19 00:06:21 +0530

    Router Hooks & Links

--- blog/src/Header.jsx
+++ blog/src/Header.jsx
@@ -1,5 +1,9 @@
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ title }) => {
+  return (
+    <header className="Header">
+      <h1>{title}</h1>
+    </header>
+  );
 };
 
 export default Header;
--- blog/src/Home.jsx
+++ blog/src/Home.jsx
@@ -1,5 +1,15 @@
-const Home = () => {
-  return <div>Home</div>;
+import Feed from "./Feed";
+
+const Home = ({ posts }) => {
+  return (
+    <main className="Home">
+      {posts.length ? (
+        <Feed posts={posts} />
+      ) : (
+        <p style={{ marginTop: "2rem" }}>No posts to display.</p>
+      )}
+    </main>
+  );
 };
 
 export default Home;
